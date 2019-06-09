36cb2e19648476f0584a9c2b7fb802ac25bba3db
AndresBlandon47 <afblandona@unal.edu.co>
2019-06-08 22:42:28 -0500

    jajajaj

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const hbs = require('hbs');
+const bodyParser = require('body-parser');
+//require ('./helpers');
+
+const directoriopublico = path.join(__dirname,'../public');
+const directoriopartials =path.join(__dirname,'../partials');
+app.use(express.static(directoriopublico));
+hbs.registerPartials(directoriopartials);
+app.use(bodyParser.urlencoded({extended: false }));
+
+app.set('view engine','hbs');
+
+app.get('/',(req, res)=>{
+    res.render('index');
+});
+
+app.listen(3000,() =>{
+    console.log('Escuchando el puerto 3000');
+})
