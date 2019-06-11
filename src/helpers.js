288ab2a835eb5c21e82c3734663e558db72f536a
diefgallegorio <diefgallegorio@unal.edu.co>
2019-06-10 23:42:12 -0500

    Diego 10/06/19

--- src/helpers.js
+++ src/helpers.js
@@ -41,5 +41,80 @@ hbs.registerHelper('verificarSesion', (usuario, pass)=>{
             }
         }
     }
+    if(texto=='aspirante'){
+        return res.redirect('/aspirante'); 
+    }
     return texto;
 })
+
+
+//Listando cursos con el collapse
+
+hbs.registerHelper('listar2',()=>{
+    listarCursos = require('./listadoCursos.json');
+    let texto = "<div class='accordion text-center' id='accordionExample'>";
+    i = 1;
+    let lista = listarCursos.filter(cur=>cur.estado == 'disponible');
+    if (lista.length == 0){
+        texto = '<h2>En el momento no hay cursos disponibles.</h2>';
+    }else{
+        lista.forEach(curso1 => {
+            texto = texto +
+            `<div class="card">
+                <div class="card-header" id="heading${i}">
+                    <h2 class="mb-0">
+                    <button class="btn btn-sucess" type="button" data-toggle="collapse" data-target="#collapse${i}" aria-expanded="true" aria-controls="collapse${i}">
+                    Curso: <b>${curso1.nombre}</b><br>Descripción: ${curso1.descripcion}<br>Valor: ${curso1.valor}.
+                    </button>
+                    </h2>
+                </div>
+
+                <div id="collapse${i}" class="collapse" aria-labelledby="heading${i}" data-parent="#accordionExample">
+                    <div class="card-body">
+                    Modalidad: ${curso1.modalidad}<br> Descripción: ${curso1.descripcion}<br>Intensidad horaria: ${curso1.intensidad} horas a la semana.
+                    </div>
+                </div>
+            </div>`
+            i=i+1;
+        });
+        texto = texto + '</div>';
+    }
+    return texto;
+});
+
+
+hbs.registerHelper('listarInscrito',()=>{
+    listarCursos = require('./listadoCursos.json');
+     
+    let lista1 = listarCursos.filter(cur=>cur.estado == 'disponible');
+    //Aqui tengo que modificar dependiendo del usuario que entre
+    let lista = lista1.filter(ins=>ins.nombrEst == 'Andres');
+    let texto = "<div class='accordion' id='accordionListarInscrito'>";
+    if (lista.length == 0){
+        texto = '<h2>En el momento no hay cursos disponibles.</h2>';
+    }else{
+        i = 1; 
+        lista.forEach(curso => {
+            texto = texto + 
+            `<div class="card">
+            <div class="card-header" id="heading${i}">
+                <h2 class="mb-0">
+                <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapse${i}" aria-expanded="true" aria-controls="collapse${1}">
+                ${curso.nombre}
+                </button>
+                </h2>
+            </div>
+            <div id="collapse${i}" class="collapse" aria-labelledby="heading${i}" data-parent="#accordionListarInscrito">
+            <div class="card-body">
+            ${curso.descripcion}<br>
+            Tiene un valor de ${curso.valor}<br>
+            y tiene los siguientes estudiantes ${curso.nombrEst}<br>
+            </div>
+            </div>`
+            
+            i = i + 1;
+        });
+    }
+    texto = texto +'</div>'
+    return texto;
+  });
